61cbb36c5a86f433dcacd1fdf1ea1c06e5fcab1a
kausik <kausiknelge@gmail.com>
2025-08-16 01:20:30 +0530

    fix: import axios

---
+++ src/pages/CyberDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+function MyComponent() {
+    // Component logic here
+
+    return (
+        <div>
+            {/* Component JSX here */}
+        </div>
+    );
+}
+
+export default MyComponent;
